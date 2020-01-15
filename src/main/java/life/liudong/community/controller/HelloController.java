c3a81511dd3c7fa8180404aee56607d9803d606d
liudong <2351036454@qq.com>
2020-01-15 14:47:26 +0800

    Initial commit

---
+++ src/main/java/life/liudong/community/controller/HelloController.java
@@ -0,0 +1,15 @@
+package life.liudong.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name = "name",required = false,defaultValue = "hello") String name , Model model)
+    {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
