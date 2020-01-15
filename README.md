bca63e00e44f65b4dc16929dee9fcaed66b4cc4a
liudong <2351036454@qq.com>
2020-01-15 16:45:25 +0800

    完成主页导航栏

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ##资料
 https://spring.io/guides
-
+https://v3.bootcss.com/components/#progress
 ##工具
--- src/main/java/life/liudong/community/controller/IndexController.java
+++ src/main/java/life/liudong/community/controller/IndexController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
+public class IndexController {
+    @GetMapping("/")
     public String greeting(@RequestParam(name = "name",required = false,defaultValue = "hello") String name , Model model)
     {
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
