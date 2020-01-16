5915dd780f1efd10fbad301a58dd3296e6cf6f9d
liudong <2351036454@qq.com>
2020-01-16 10:37:50 +0800

    github授权信息配置文件化

--- src/main/java/life/liudong/community/controller/AuthorizeController.java
+++ src/main/java/life/liudong/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.liudong.community.dto.AccessTokenDTO;
 import life.liudong.community.dto.GithubUser;
 import life.liudong.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,16 +14,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String cliendId;
+
+    @Value("${github.client.secret}")
+    private String cliendSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,@RequestParam(name="state") String state) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("40cca80d10082a99e683");
-        accessTokenDTO.setClient_secret("798ad8dbcc240e8a8392dc6cbb605d48c2701ce8");
+        accessTokenDTO.setClient_id(cliendId);
+        accessTokenDTO.setClient_secret(cliendSecret);
 
         String accessToken=githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user=githubProvider.getUser(accessToken);
