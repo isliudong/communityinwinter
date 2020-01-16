0cff0b315aa47db84d2bc38d7d32cc0182d950e2
liudong <2351036454@qq.com>
2020-01-16 14:03:23 +0800

    添加h2数据库支持

--- README.md
+++ README.md
@@ -2,18 +2,35 @@
 
 ##资料
 [文档](https://spring.io/guides)
+
 [文档](https://v3.bootcss.com/components/#progress)
+
+[git授权](https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/)
 ##工具
 
 ##技术点
 [OKHttp模拟post请求](https://square.github.io/okhttp/)
-
+h2微型数据库
+jpa是一种持久层规范，hibernate实现了它
+Hibernate的DAO层开发比MyBatis简单，Mybatis需要维护SQL和结果映射。
+Hibernate对对象的维护和缓存要比MyBatis好，对增删改查的对象的维护要方便。
+Hibernate数据库移植性很好，MyBatis的数据库移植性不好，不同的数据库需要写不同SQL。
+Hibernate有更好的二级缓存机制，可以使用第三方缓存。MyBatis本身提供的缓存机制不佳
+ 
+mybatis是另一种持久方案的实现
+MyBatis可以进行更为细致的SQL优化，可以减少查询字段。
+MyBatis容易掌握，而Hibernate门槛较高。
 ##快捷方式
 文件搜索：ctrl+shift+n
+
 编辑最大化：ctrl+shift+f12
+
 非截断换行：shift+enter
+
 浏览器匿名无个人信息：ctrl+shift+n
+
 快速匿名转显：ctrl+alt+v
+
 重载参数查看：ctrl+
 ##问题
 [OKHttp异常java.lang.IllegalStateException: closed](https://blog.csdn.net/u012587005/article/details/78504925)
--- src/main/java/life/liudong/community/controller/AuthorizeController.java
+++ src/main/java/life/liudong/community/controller/AuthorizeController.java
@@ -5,9 +5,12 @@ import life.liudong.community.dto.GithubUser;
 import life.liudong.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -24,7 +27,7 @@ public class AuthorizeController {
     private String redirectUri;
 
     @RequestMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name="state") String state) {
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name="state") String state, HttpServletRequest request) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -36,6 +39,15 @@ public class AuthorizeController {
         String accessToken=githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user=githubProvider.getUser(accessToken);
         System.out.println(user.getName());
-        return "index";
+        if(user!=null)
+        {
+            //登录成功写入session
+            request.getSession().setAttribute("user",user);
+            return "redirect:";//转发至主页，可以去掉链接后缀信息
+        }else {
+            return "redirect:";
+            //登录失败
+        }
+
     }
 }
