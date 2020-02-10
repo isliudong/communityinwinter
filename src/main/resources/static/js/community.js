e5e5d65235a05374e6e3d2c7b6f4952be3e1a2a7
liudong <2351036454@qq.com>
2020-02-10 11:26:23 +0800

    完成前端子评论

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,9 +1,10 @@
+//提交回复
 function post() {
 
     let questionId = $("#question_id").val();
     let content = $("#comment_content").val();
 
-    if (!content){
+    if (!content) {
         alert("内容为空~");
         return;
     }
@@ -25,9 +26,9 @@ function post() {
                 if (response.code == 2003) {
 
                     var isAccept = confirm(response.message);
-                    if (isAccept){
+                    if (isAccept) {
                         window.open("https://github.com/login/oauth/authorize?client_id=40cca80d10082a99e683&redirect_uri=http://localhost:8080/callback&scope=user&state=1")
-                        window.localStorage.setItem("isClose",true);
+                        window.localStorage.setItem("isClose", true);
                     }
                 } else {
                     alert(response.message)
@@ -37,4 +38,28 @@ function post() {
         },
         dataType: "json"
     });
+}
+
+//展开二级评论
+function collapseComments(e) {
+
+    let id = e.getAttribute("data-id");
+    let comments = $("#comment-" + id);
+    //获取二级评论展开状态
+    let collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+        //关闭二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    }
+
+    else {
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse", "in");
+        e.classList.add("active");
+    }
+
 }
