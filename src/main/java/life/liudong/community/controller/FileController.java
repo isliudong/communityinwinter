285219694e163dfa8b26a89cb03703191b8bf6cc
liudong <2351036454@qq.com>
2020-02-24 21:18:02 +0800

    完成图片上传

--- src/main/java/life/liudong/community/controller/FileController.java
+++ src/main/java/life/liudong/community/controller/FileController.java
@@ -1,11 +1,20 @@
 package life.liudong.community.controller;
 
 import life.liudong.community.dto.FileDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.UUID;
 
 /**
  * @program: community
@@ -15,12 +24,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
  **/
 @Controller
 public class FileController {
+    @Value("${image.sever.url}")
+    private String imgSever;
+
+
     @ResponseBody
     @RequestMapping("/file/upload")
-    public FileDTO upload(){
+    public FileDTO upload(HttpServletRequest request){
+
+        MultipartHttpServletRequest multipartHttpServletRequest= (MultipartHttpServletRequest) request;
+        MultipartFile file=multipartHttpServletRequest.getFile("editormd-image-file");
+
+
+
+        //得到上传时的原文件名
+        String originalFilename = file.getOriginalFilename();
+        //获取文件后缀名
+        String suffixName = originalFilename.substring(originalFilename.lastIndexOf("."));
+        //获取文件格式
+        String ext = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+        //获取uuid作为文件名
+        String name = UUID.randomUUID().toString().replaceAll("-", "");
+        String saveName = name + suffixName;
+        //虚拟存储服务器
+        String picDir = imgSever;
+        //图片存储全路径
+        String outputPath = picDir + '\\' + saveName;
+
+        try {
+            File saveFile = new File(outputPath);
+            file.transferTo(saveFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
-        fileDTO.setUrl("/img/tuijianimg.jpg");
+        fileDTO.setMessage("success");
+        fileDTO.setUrl("/img/"+saveName);
         return fileDTO;
     }
 }
