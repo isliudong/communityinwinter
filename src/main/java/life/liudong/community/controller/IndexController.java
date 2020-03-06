84cef06a4ae417622fbe4a5ca863ed1840e054b8
liudong <2351036454@qq.com>
2020-03-06 22:10:33 +0800

    配置化授权应用信息

--- src/main/java/life/liudong/community/controller/IndexController.java
+++ src/main/java/life/liudong/community/controller/IndexController.java
@@ -4,6 +4,7 @@ import life.liudong.community.dto.PaginationDTO;
 import life.liudong.community.dto.QuestionDTO;
 import life.liudong.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,8 @@ public class IndexController {
 
     @Autowired
     QuestionService questionService;
+    @Value("${github.client.id}")
+    String clientId;
     @GetMapping("/")
     public String index(Model model,
                         @RequestParam(name = "page",defaultValue = "1") Integer page,
@@ -24,6 +27,7 @@ public class IndexController {
         PaginationDTO<QuestionDTO> pagination=questionService.list(search,page,size);
         model.addAttribute("pagination",pagination);
         model.addAttribute("search",search);
+        model.addAttribute("clientId",clientId);
 
         return "index";
     }
