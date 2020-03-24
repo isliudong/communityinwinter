526059f65c17a4dc0de3ddcf8638fd3c8216283d
liudong <2351036454@qq.com>
2020-03-24 21:26:44 +0800

    优化登录验证

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,4 +1,5 @@
 function r(){
+
     $.ajax({
         url : '/verify',
         contentType: "application/json",
@@ -15,4 +16,16 @@ function r(){
             $('#msg').text(data.message);
         }
     })
+}
+function verifyPwd() {
+    let pwd = $("input[name='password']").val();
+    let pwd2 = $("input[name='password2']").val();
+    if (pwd!==pwd2){
+        $('#pwdMsg').removeClass('display');
+        $('#pwdMsg').show();
+        $('#pwdMsg').text("两次输入的密码不相同");
+    }
+    if (pwd===pwd2){
+        $('#pwdMsg').hide();
+    }
 }
